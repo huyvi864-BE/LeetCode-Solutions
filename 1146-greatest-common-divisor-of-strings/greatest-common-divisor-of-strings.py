1d2cd32a0c525c08700c6eb6c5d1dfba6156a35c
Vi Quang Huy <huyvi864@gmail.com>
2026-03-17 23:21:53 +0700

    Time: 1 ms (32.12%) | Memory: 12.5 MB (22.64%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if len(str1) < len(str2):
+            return self.gcdOfStrings(str2, str1)
+        if not str2:
+            return str1
+        if not str1.startswith(str2):
+            return ""
+        remaining = str1[len(str2):]
+        return self.gcdOfStrings(remaining, str2)
